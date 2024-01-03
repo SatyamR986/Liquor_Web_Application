37a7240f1961bb30f570fa8a305e56111baea242
Satyam <sregmi986@gmail.com>
2024-01-03 21:18:49 +0545

    Added breadcrumbs functionality

--- src/Components/BreadCrumbs/BreadCrumb.jsx
+++ src/Components/BreadCrumbs/BreadCrumb.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 import './BreadCrumb.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-// const BreadCrumb = (props) => {
-//     const {product}= props;
-//     console.log(product);
-//   return (
-//     <div className='breadcrumb'>
-//         HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
-//     </div>
-//   )
-// }
-
 const BreadCrumb = (props) => {
     const { product } = props;
     const category = product ? product.category : 'Unknown';
@@ -22,6 +12,17 @@ const BreadCrumb = (props) => {
         </div>
     );
 }
+// const BreadCrumb = (props) => {
+//     const {product}= props;
+//     console.log(product);
+//   return (
+//     <div className='breadcrumb'>
+//         HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+//     </div>
+//   )
+// }
+
+
 
 
 export default BreadCrumb
